4654e4dd3126e07ecced99ceb690a91f51480244
hasina <hasijaosoah@gmail.com>
2024-11-16 15:26:09 +0300

    first

---
+++ Views/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Publics/assets/css/style.css">
+    <link rel="stylesheet" href="../Publics/assets/css/bootstrap.min.css">
+    <title>Mon projet</title>
+</head>
+
+<body>
+
+    <h1>Bonjour tout le monde</h1>
+
+    <script src="../Publics/assets/js/bootstrap.min.js"></script>
+    <script src="main.js"></script>
+</body>
+
+</html>
